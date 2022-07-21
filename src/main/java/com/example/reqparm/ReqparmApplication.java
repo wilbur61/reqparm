2360d40322dc9edb1b36124e75d104dcd9bacc74
wilbur61 <wroales@perscholas.org>
2022-07-21 09:56:37 -0400

    Added my project

---
+++ src/main/java/com/example/reqparm/ReqparmApplication.java
@@ -0,0 +1,17 @@
+package com.example.reqparm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//
+// https://bushansirgur.in/spring-boot-requestparam-annotation-example/
+//
+
+@SpringBootApplication
+public class ReqparmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReqparmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/reqparm/ReqparmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reqparm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReqparmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
